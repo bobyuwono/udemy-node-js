a75f950218e983097f490726bb58bba583b3c33d
Bob Yuwono <39585526+bobyuwono@users.noreply.github.com>
2020-01-18 01:40:50 +0700

    Storing data with JSON
    video section 4 nomor 18

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,19 +1,46 @@
-// const fs = require('fs');
-
-// fs.writeFileSync('notes.txt','hula hula');
-// fs.appendFileSync('notes.txt','\ndata to append');
-
-// const add  = require ('./utils.js');
-// const sum  = add (4,-2);;
-// // console.log(sum);
-// const validator = require('validator')
 const getNotes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-const msg = getNotes();
-console.log(msg);
+// customizwe yargs version 
+yargs.version('1.1.0');
 
-const greenMSG = chalk.green.inverse.bold ("Success!");
-console.log(greenMSG);
-console.log(chalk.bgRed('Hello world!'));
-console.log(chalk.bgRed('taki taki!'));
+//adding add command
+yargs.command({
+    command: 'add',
+    describe: "add a new note",
+    builder:{
+        title: {
+            describe: 'Note title', 
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: "Note body",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ' + argv.title + "\nBody: "+ argv.body);
+    }
+})
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note', 
+    handler: function(){
+    console.log('Removing the note..');
+    }
+})
+//create list command
+yargs.command({
+    command: "list",
+    describe: 'List your notes',
+    handler: function(){
+        console.log("Listing out all notes")
+    }
+})
+
+yargs.parse()
+
---
+++ playground/1-json.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const dataBob = {
+    name: 'Bob Yuwono',
+    planet: 'Earth',   
+    age: '20',
+}
+
+
+const dataBufffer = fs.readFileSync('1-json.json'); //nge load json nya sebagai buffer
+const dataJSON = dataBufffer.toString(); //buffernya diubah jadi string
+const data = JSON.parse(dataJSON); //stringnya di-parse biar jadi objek
+
+const dataBobJSON = JSON.stringify(dataBob);
+fs.writeFileSync('1-json.json',dataBobJSON);
+
+
+console.log(dataBob.name);
