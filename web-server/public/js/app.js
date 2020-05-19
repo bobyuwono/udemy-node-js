19c577fe72e842c4466247513b66d0baf2bcccba
Bob Yuwono <39585526+bobyuwono@users.noreply.github.com>
2020-05-19 13:42:11 +0700

    Browser http Fetch request

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('client side javascript is loaded!')
+
+
+fetch('http://localhost:3000/weather?address=!').then((response) => {
+    response.json().then((data => {
+        if(data.error) {console.log(data.error)}
+        else { 
+            console.log(data.location)
+            console.log(data.forecastData)
+        } 
+    }))
+})
--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -7,14 +7,14 @@ const geocode = (address,callback) => {
     request({url ,json:true}, (error,{body})=>{
         if (error){
             // error if there's no connection
-            callback('unable to connect to location servies!',undefined)
+            callback('Unable to connect to location servies!',undefined)
         } else if(body.features.length === 0){
             // error if there's no matching location
             callback('Unable to find location. Try another search.',undefined)
         }else{
             // running properly
             callback(undefined, {
-                longitude:  body.features[0].center[0],
+                longitude: body.features[0].center[0],
                 latitude: body.features[0].center[1],
                 location: body.features[0].place_name  
             })
