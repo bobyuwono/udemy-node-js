e3090fc96971cbed6656b590fbfd104e7a0658af
Bob Yuwono <39585526+bobyuwono@users.noreply.github.com>
2020-05-22 15:30:11 +0700

    59. Wiring up the User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,32 +1,34 @@
 console.log('client side javascript is loaded!')
 
-
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data => {
-        if(data.error) {console.log(data.error)}
-        else { 
-            console.log(data.location)
-            console.log(data.forecastData)
-        } 
-    }))
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    // providing loading message
+    messageOne.textContent = "Loading.. please wait"
+    messageTwo.textContent = ""
+
     const location = search.value
     fetch('http://localhost:3000/weather?address='+location).then((response) => {
         response.json().then((data => {
-            if(data.error) {console.log(data.error)}
+            if(data.error) {
+                console.log(data.error)
+                messageOne.textContent =data.error
+            }
             else { 
                 console.log(data.location)
                 console.log(data.forecastData)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecastData.summary +data.forecastData.temperature
             } 
         }))
     })
-    
 
     console.log(location)
 })
+
+
